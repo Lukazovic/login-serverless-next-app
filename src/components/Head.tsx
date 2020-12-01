337f0b08e7a8cedcd7f4da5b5288b72e6f7aa3c7
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-30 22:42:32 -0300

    Update head component

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import NextHead from "next/head";
+import React from 'react';
+import NextHead from 'next/head';
 
 interface IProps {
   title: string;
   faviconName?: string;
 }
 
-const Head: React.FC<IProps> = ({ title, faviconName = "favicon" }) => (
+const Head: React.FC<IProps> = ({ title, faviconName = 'favicon' }) => (
   <NextHead>
     <title>{title}</title>
     <link rel="icon" href={`/${faviconName}.ico`} />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />
   </NextHead>
 );
 
