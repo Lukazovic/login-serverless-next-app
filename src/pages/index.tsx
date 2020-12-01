ab563cd3fa42f4b14daefea4c61148da9393fba5
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-30 22:43:19 -0300

    Add navbar and head to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import Head from "../components/Head";
+import Head from '../components/Head';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Head title="Login Serveless" />
+
+      <Navbar />
 
       <h1>Home</h1>
     </div>
